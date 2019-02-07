b2cccd4d1d1f03f68e2fcd672b258ae7bf9e6f58
Eric Le Maître <eric.erwan.le.maitre@gmail.com>
2019-02-07 09:38:16 +0100

    Add twitterURL to User and make Categories unqiue

---
+++ Sources/App/Migrations/07-02-2019-MakeCategoriesUnique.swift
@@ -0,0 +1,18 @@
+import FluentPostgreSQL
+import Vapor
+
+struct MakeCategoriesUnique: Migration {
+  typealias Database = PostgreSQLDatabase
+  
+  static func prepare(on conn: PostgreSQLConnection) -> EventLoopFuture<Void> {
+    return Database.update(Category.self, on: conn, closure: { (builder) in
+      builder.unique(on: \.name)
+    })
+  }
+  
+  static func revert(on conn: PostgreSQLConnection) -> EventLoopFuture<Void> {
+    return Database.update(Category.self, on: conn, closure: { (builder) in
+      builder.deleteUnique(from: \.name)
+    })
+  }
+}
